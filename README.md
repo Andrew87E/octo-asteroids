8200f577980186b602d066d11108d8d766841d12
Andrew Edwards <aedwards@tifco.com>
2023-12-29 15:56:17 -0600

    updated readme added some tips and controls

--- README.md
+++ README.md
@@ -1,40 +1,57 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Asteroids
+
+This project is a modern take on the classic Asteroids arcade game, developed using TypeScript and React. Players navigate a spaceship, dodging and shooting asteroids while progressing through levels.
+
+## Features
+
+- Classic Asteroids gameplay with a modern twist.
+- Progress through levels, each with increasing difficulty.
+- Health system with heart icons representing the player's lives.
+- Responsive canvas rendering for a smooth gaming experience.
 
 ## Getting Started
 
-First, run the development server:
+To get a local copy up and running, follow these simple steps.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+### Prerequisites
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- npm
+  ```sh
+  npm install npm@latest -g
+  ```
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+### Installation
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+1. Clone the repo
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+   ```sh
+   git clone https://github.com/andrew87e/octo-asteroids.git
+   ```
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+2. Install NPM packages
 
-## Learn More
+   ```sh
+   npm install
+   ```
 
-To learn more about Next.js, take a look at the following resources:
+3. Start the development server
+   ```sh
+   npm start
+   ```
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+Navigate through the game using keyboard controls. Avoid asteroids and shoot them down to progress through levels.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+# Contributing
 
-## Deploy on Vercel
+Contributions are what make the open-source community such an amazing place to learn, inspire, and create. Any contributions you make are greatly appreciated.
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+    Fork the Project
+    Create your Feature Branch (git checkout -b feature/AmazingFeature)
+    Commit your Changes (git commit -m 'Add some AmazingFeature')
+    Push to the Branch (git push origin feature/AmazingFeature)
+    Open a Pull Request
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+# License
+
+Distributed under the MIT License. See LICENSE for more information.
+
--- pages/game.tsx
+++ pages/game.tsx
@@ -1,17 +1,108 @@
+import { useState } from "react";
 import Link from "next/link";
 import GameCanvas from "../components/gameCanvas";
 
 const GamePage = () => {
+  const [showControls, setShowControls] = useState(false);
+  const [showHowToPlay, setShowHowToPlay] = useState(false);
+  const [showScoring, setShowScoring] = useState(false);
+  const [showTips, setShowTips] = useState(false);
+
+  const toggleControls = () => setShowControls(!showControls);
+  const toggleHowToPlay = () => setShowHowToPlay(!showHowToPlay);
+  const toggleScoring = () => setShowScoring(!showScoring);
+  const toggleTips = () => setShowTips(!showTips);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
       <div className="mb-4">
         <h1 className="text-3xl font-bold">Asteroids Game</h1>
       </div>
-      
-        <GameCanvas />
-      
+
+      <GameCanvas />
+
       <div className="mb-6">
-        <p>Game controls and information will go here</p>
+        <h2
+          className="text-xl font-bold mb-2 cursor-pointer"
+          onClick={toggleControls}
+        >
+          Game Controls
+        </h2>
+        {showControls && (
+          <div>
+            <p>Use the following keyboard controls to play the game:</p>
+            <ul className="list-disc list-inside mb-4">
+              <li>
+                <strong>Arrow Up/W:</strong> Move forward
+              </li>
+              <li>
+                <strong>Arrow Left/A:</strong> Rotate left
+              </li>
+              <li>
+                <strong>Arrow Right/D:</strong> Rotate right
+              </li>
+              <li>
+                <strong>Spacebar:</strong> Shoot
+              </li>
+            </ul>
+          </div>
+        )}
+
+        <h2
+          className="text-xl font-bold mb-2 cursor-pointer"
+          onClick={toggleHowToPlay}
+        >
+          How to Play
+        </h2>
+        {showHowToPlay && (
+          <div>
+            <p>
+              Navigate your spaceship through space and avoid colliding with
+              asteroids. Use your weapons to shoot and destroy asteroids. Each
+              level gets progressively harder.
+            </p>
+            <p>
+              Keep an eye on your health, represented by hearts at the top of
+              the screen. If you run out of health, it's game over!
+            </p>
+          </div>
+        )}
+
+        <h2
+          className="text-xl font-bold mb-2 cursor-pointer"
+          onClick={toggleScoring}
+        >
+          Scoring
+        </h2>
+        {showScoring && (
+          <div>
+            <p>
+              Earn points by destroying asteroids. The faster you clear the
+              asteroids, the higher your score!
+            </p>
+          </div>
+        )}
+
+        <h2
+          className="text-xl font-bold mb-2 cursor-pointer"
+          onClick={toggleTips}
+        >
+          Tips
+        </h2>
+        {showTips && (
+          <div>
+            <ul className="list-disc list-inside mb-4">
+              <li>Move strategically and don't get cornered by asteroids.</li>
+              <li>
+                Use short bursts of movement to dodge asteroids effectively.
+              </li>
+              <li>
+                Regularly check for asteroids appearing from the edges of the
+                screen.
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
 
       <Link
